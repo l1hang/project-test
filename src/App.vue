ad24476ae55f75b5475dc0a2f20327efb51ea21b
l1hang <qaz1995xyz@163.com>
2024-05-16 16:44:37 +0800

    feat:时钟翻牌器

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <h1>测试下拉树状组件</h1>
-    <Button>Default</Button>
-    <Button type="primary">Primary</Button>
-    <base-select-tree  />
+    <timeFilp />
   </div>
 </template>
 
 <script>
-import baseSelectTree from "./components/base-select-tree.vue";
+import timeFilp from "./components/timeFilp.vue";
 export default {
   components: {
-    baseSelectTree,
+    timeFilp,
   },
   name: "App",
   data() {
@@ -20,17 +17,7 @@ export default {
     };
   },
   methods: {
-    clickChange(val) {
-      console.log(val);
-    },
-    //点击执行科室树节点
-    clickNodeChange(data) {
-      this.typeCodeList.push({
-        value: data[0].value,
-        label: data[0].title,
-      });
-      this.formData.deptID.push(data[0].value);
-    },
+
   },
 };
 </script>
